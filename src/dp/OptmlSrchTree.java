e34b9f2f220f1b937ba7f7857e2527aa1f44ffdb
colm-mchugh <colm.mchugh@gmail.com>
2018-02-13 23:23:33 -0800

    Document Sequence alignment, optimal search tree

--- src/dp/OptmlSrchTree.java
+++ src/dp/OptmlSrchTree.java
@@ -3,14 +3,37 @@ package dp;
 /**
  * Compute the optimal cost of a binary search tree.
  * 
- * "Why ?" If a set of search keys have frequencies, then organizing them as a
+ * If a set of search keys have frequencies, then organizing them as a
  * perfectly balanced search tree may be sub-optimal in terms of the size of 
  * tree traversals. It may be more efficient to have most frequently searched
  * keys near the root of a tree, with the consequence of an unbalanced tree.
  * 
- * "How ?"
+ * Goal: a valid binary search trees that minimizes the weighted average search time,
+ * which is: SUM pi X time to find i
+ * where pi is the frequency of item i and time to find i is effectively the 
+ * depth of item i in the tree.
  * 
- * @author colm_mchugh
+ * Optimal substructure: an optimal search tree with r as root has an optimal
+ * left subtree for the items 1 .. r-1 and an optimal right subtree for the 
+ * items r+1 .. N 
+ * 
+ * The items in a subproblem are either in a prefix or a suffix. An algorithm 
+ * needs to compute the OST for these subsets of S : { 1,2,3….,N } all contiguous 
+ * intervals S: { i, i+1, …, j-1, j } for every i, j in 1..N because of multiple 
+ * levels of recursion.
+ * 
+ * Recurrence: for all i, j in 1..N with i less than or equal to j, 
+ *  Oij = MIN r ( SUM Pk + Oir-1 + Or+1j ) for k in i..j, r in i..j
+ * 
+ * Algorithm: (Solve smallest subsets first (fewest items j - i + 1))
+ * A[i,j] = OST value for items { i, .., j }
+ * for s in 0 .. (N - 1)  (S represents (j - i))
+ *   for i in 1..N
+ *      A[i,i+s] = MIN r ( SUM Pk + A[i, r-1] + A[r + 1, i + s] ) for k in i .. i + s, r in i .. i + s
+ * 
+ * Running time: O(NxN) subproblems with O(j - 1) time to compute each subproblem gives O(N^3) overall
+ * 
+ * TODO: implement a DP algorithm. This implementation uses recursion.
  */
 public class OptmlSrchTree {
 
--- src/dp/SequenceAlignment.java
+++ src/dp/SequenceAlignment.java
@@ -21,37 +21,40 @@ public class SequenceAlignment {
         return (l1 == l2 ? 1 : 0);
     }
 
+    // Alignment holds all the state for producing an alignment between two 
+    // strings X and Y. 
     static class Alignment {
-
         int needleman_wunsch_score;
         int edit_distance;
         StringBuilder alignX;
         StringBuilder alignY;
         int i, j;
 
-        private Alignment(String x, String y) {
+        private Alignment(String x, String y, int needleman_wunsch_score) {
             this.alignX = new StringBuilder();
             this.alignY = new StringBuilder();
             this.i = x.length();
             this.j = y.length();
             this.edit_distance = 0;
+            this.needleman_wunsch_score = needleman_wunsch_score;
         }
         
-        public void case1(char x, char y) {
+        public void match(char x, char y) {
+            assert x == y; // only makes sense when x == y
             alignX.insert(0, x);
             alignY.insert(0, y);
             this.i--;
             this.j--;
         }
         
-        public void case2(char x) {
+        public void xWithGap(char x) {
             alignX.insert(0, x);
             alignY.insert(0, GAP);
             this.i--;
             this.edit_distance++;
         }
         
-        public void case3(char y) {
+        public void yWithGap(char y) {
             alignX.insert(0, GAP);
             alignY.insert(0, y);
             this.j--;
@@ -63,12 +66,21 @@ public class SequenceAlignment {
         int M = X.length();
         int N = Y.length();
         int[][] p_memo = new int[M + 1][N + 1];
+        // First part: build and populate the penalty memo
+        
+        // The penalty for matching each prefix of X with the empty string is
+        // the length of the prefix times the penalty for a gap, and likewise
+        // for matching each prefix of Y with the empty string.
         for (int i = 0; i <= M; i++) {
             p_memo[i][0] = P_GAP * i;
         }
         for (int j = 0; j <= N; j++) {
             p_memo[0][j] = P_GAP * j;
         }
+        // Determine the penalty for each slot by taking the maximum of;
+        //  1) the cost of an X[i], Y[j] match with the previous penalty
+        //  2) the cost of matching X[i] with a gap
+        //  3) the cost of matching Y[j] with a gap
         for (int i = 1; i <= M; i++) {
             for (int j = 1; j <= N; j++) {
                 int xy = p_memo[i - 1][j - 1] + p_letter(X.charAt(i - 1), Y.charAt(j - 1));
@@ -77,26 +89,31 @@ public class SequenceAlignment {
                 p_memo[i][j] = Integer.max(Integer.max(xy, yGap), xGap);
             }
         }
-        Alignment align = new Alignment(X, Y);
+        
+        // Second part: build the alignment of X with Y.
+        //
+        // Working backwards from entry [M][N] of the memo, determine which option 
+        // 1), 2) or 3) was used to compute each entry.
+        Alignment align = new Alignment(X, Y, p_memo[M][N]);
         while (align.i > 0 && align.j > 0) {
             int i = align.i;
             int j = align.j;
             if (p_memo[i][j] == p_memo[i - 1][j] + P_GAP) {
-                align.case2(X.charAt(i - 1));
+                align.xWithGap(X.charAt(i - 1));
             } else if (p_memo[i][j] == p_memo[i - 1][j - 1] + p_letter(X.charAt(i - 1), Y.charAt(j - 1))) {
-                align.case1(X.charAt(i - 1), Y.charAt(j - 1));
+                align.match(X.charAt(i - 1), Y.charAt(j - 1));
             } else {
-                align.case3(Y.charAt(j - 1));
+                align.yWithGap(Y.charAt(j - 1));
             }
         }
+        // If not all characters in X were visited, align the remaining chars with a gap.
         while (align.i > 0) {
-            align.case2(X.charAt(align.i - 1));
+            align.xWithGap(X.charAt(align.i - 1));
         }
+        // likewise for Y
         while (align.j > 0) {
-            align.case3(Y.charAt(align.j - 1));
+            align.yWithGap(Y.charAt(align.j - 1));
         }
-        System.out.println(Arrays.deepToString(p_memo));
-        align.needleman_wunsch_score = p_memo[M][N];
         return align;
     }
 }
